59d1f18010864afdc0f49e92c21d45992209b6a5
yspkm <yskim0830@outlook.com>
2023-11-25 14:14:51 +0900

    feat(): PUvicorn & Gunicron 혼합된 형태로 웹서 버 구성

---
+++ refactor/src/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -z "$WORKERS" ]; then
+  CORES=$(grep -c ^processor /proc/cpuinfo)
+  WORKERS=$(( 2 * CORES + 1 ))
+fi
+
+python3 manage.py makemigrations
+python3 manage.py migrate
+# b, w, k, t
+exec gunicorn \
+  --bind 0.0.0.0:8000 \
+  --workers ${WORKERS} \
+  --worker-class uvicorn.workers.UvicornWorker \
+  --timeout 3600 \
+  mailai.asgi:application
