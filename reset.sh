2fc6e0d2a37d6a718cc6b20aae9e477c63c0a9cc
Ubuntu <ubuntu@ip-172-26-0-68.ap-northeast-1.compute.internal>
2023-11-29 08:10:46 +0000

    feat(tools): Modify reset.sh to support quasi-Zero-downtime Deployment

--- reset.sh
+++ reset.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-#sudo docker stop doc
-sudo docker stop web 
-#sudo docker rm doc 
-sudo docker rm web 
-#sudo make build service=doc
 sudo make build service=web
-#sudo make up service=doc
+
+sudo docker stop web 
+sudo docker rm web 
 sudo make up service=web
 
 sudo make logs service=web
